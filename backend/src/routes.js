5ad0e58d7f2c30d4371c9f256a64846cd5601981
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-06-12 20:56:11 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const PostController = require('./controllers/PostController');
+
+const routes = new express.Router();
+
+routes.post('/posts', (req, res) => {
+    return res.send(`Olá ${req.query.name}`);
+});
+
+module.exports = routes;
