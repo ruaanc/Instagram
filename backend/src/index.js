9cf875959d7597e3284655c49ee6b412f7894520
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-06-12 20:56:02 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,8 @@ const mongoose = require('mongoose');
 const app = express();
 
 mongoose.connect('mongodb+srv://ruaanc:Ru97735666@cluster0-tb8s3.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true,})
-app.get('/', (req, res) => {
-    return res.send(`Olá ${req.query.name}`);
-});
+
+app.use(require('./routes'));
 
 app.listen(3333);
 
