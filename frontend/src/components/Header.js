bd928e26d9fbe7e761e431ea07cc33874a25fee5
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-06-19 21:29:41 -0300

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,14 +4,13 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 import logo from '../assets/logo1.svg';
-import camera from '../assets/camera.svg';
 
 export default function Header() {
     return (
         <header id="main-header">
             <div className="header-content">
                 <Link to="/"><img src={logo} width="100px" height="100px" alt="InstaRocket" /></Link>
-                <Link to="/new"><img src={camera}  width="50px" height="50px" alt="Enviar publicação" /></Link>
+                <Link to="/new"><img src="https://img.icons8.com/material-outlined/60/000000/camera.png" alt=""></img></Link>
                 
             </div>
         </header>
